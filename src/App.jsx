88fcaeac585055663f28317f11cbd0bd0e58c190
brandaolipe <felipebrandaochaves@gmail.com>
2024-07-19 15:24:43 -0300

    Estrutura base com header, body e footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import Header from './components/header/Header'
-import Home from './components/home/Home'
-import About from './components/about/About'
+import Header from './components/header/Header.jsx'
+import Body from './components/body/Body.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <Header />
-      <Home />
-      <About />
-    </>
+      <Body />
+      <Footer />
+    </div>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './header.css';
+import logo from '../../assets/img/logo-runner.png'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Header = () =>{
+export default function Header() {
+  
   return (
-    <div>Header</div>
+    <>
+      <div class="container-fluid">
+        <header class="row" id="header">
+          <div id="logo-container">
+            <img src={logo} alt="corredor" id="logo" />
+          </div>
+        </header>
+      </div>
+    </>
   )
 }
-
-export default Header
